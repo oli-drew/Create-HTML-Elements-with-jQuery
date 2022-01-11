f62ac01125a079159a3ff928bef04800bf56e16e
Oli Drew <oli.drew92@gmail.com>
2022-01-11 20:10:11 +0000

    Dynamically add elements and assign id

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("linked");
+// As a user, I want to be greeted with a quote by an author when I open the page.
+
+// It's done when the page dynamically displays a quote by an author and the author's name on load.
+const parent = $("body");
+const quoteElement = $("<h1>").appendTo(parent);
+quoteElement.attr("id", "quote");
+const authorElement = $("<h1>").appendTo(parent);
+authorElement.attr("id", "author");
+
+//  On page load do this
+$(document).ready(() => {
+  display("hi", "~Oli");
+});
+
+// Display on page
+const display = (quote, author) => {
+  quoteElement.text(quote);
+  authorElement.text(author);
+};
